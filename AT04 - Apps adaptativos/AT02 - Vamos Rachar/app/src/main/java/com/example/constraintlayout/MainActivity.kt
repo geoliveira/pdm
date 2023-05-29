5bf71be1e8b167e8b2cdde8af63436b3184924f4
Geovane Oliveira <geoliiveira2@gmail.com>
2023-05-29 09:09:57 -0300

    Add: AT04 - Apps Adaptativos for Vamos Rachar app

--- AT04 - Apps adaptativos/AT02 - Vamos Rachar/app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ AT04 - Apps adaptativos/AT02 - Vamos Rachar/app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -11,6 +11,10 @@ import android.widget.TextView
 import java.util.*
 import android.content.Intent
 import android.widget.Button
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), TextWatcher, TextToSpeech.OnInitListener {
@@ -20,6 +24,7 @@ class MainActivity : AppCompatActivity(), TextWatcher, TextToSpeech.OnInitListen
     private lateinit var tvResultado: TextView
     private lateinit var fabCompartilhar: FloatingActionButton
     private lateinit var btFalar: Button
+    private lateinit var swModoNoturno: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +35,17 @@ class MainActivity : AppCompatActivity(), TextWatcher, TextToSpeech.OnInitListen
         tvResultado = findViewById(R.id.tvResultado)
         fabCompartilhar = findViewById(R.id.fabCompartilhar)
         btFalar = findViewById(R.id.btFalar)
+        swModoNoturno = findViewById(R.id.modoNoturno)
 
         edtConta.addTextChangedListener(this)
         edtPessoas.addTextChangedListener(this)
-
         // Initialize TTS engine
         tts = TextToSpeech(this, this)
+
+        swModoNoturno.setOnCheckedChangeListener({ _ , isChecked ->
+            setDefaultNightMode(if (isChecked) MODE_NIGHT_YES else MODE_NIGHT_NO)
+        })
+
     }
 
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -49,14 +59,31 @@ class MainActivity : AppCompatActivity(), TextWatcher, TextToSpeech.OnInitListen
     override fun afterTextChanged(s: Editable?) {
         var valor: String = calculate()
         if (!valor.isEmpty()) {
-            tvResultado.setText("R$ "+valor+" para cada um")
-
+            if (Locale.getDefault().language == "en") {
+                tvResultado.setText("$ " + valor + " for each one")
+            } else {
+                tvResultado.setText("R$ " + valor + " para cada um")
+            }
             fabCompartilhar.setOnClickListener {
                 val intent = Intent()
                 intent.action = Intent.ACTION_SEND
-                intent.putExtra(Intent.EXTRA_TEXT, "O valor da conta fica R$ $valor para cada um")
+                if (Locale.getDefault().language != "en") {
+                    intent.putExtra(
+                        Intent.EXTRA_TEXT,
+                        "Each one has to pay $ $valor"
+                    )
+                } else {
+                    intent.putExtra(
+                        Intent.EXTRA_TEXT,
+                        "O valor da conta fica R$ $valor para cada um"
+                    )
+                }
                 intent.type = "text/plain"
-                startActivity(Intent.createChooser(intent, "Compartilhar com:"))
+                if (Locale.getDefault().language == "en") {
+                    startActivity(Intent.createChooser(intent, "Compartilhar com:"))
+                } else {
+                    startActivity(Intent.createChooser(intent, "Share with:"))
+                }
             }
 
             btFalar.setOnClickListener {
